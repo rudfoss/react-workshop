edd4b2a7619e92252501df7b7ee6cdb3f1c550e4
Thomas <thomas.rudfoss@gmail.com>
2019-04-03 10:57:32 +0200

    Controlled form

--- src/features/userForm/UserForm.js
+++ src/features/userForm/UserForm.js
@@ -2,12 +2,29 @@ import React from "react"
 import PropTypes from "prop-types"
 
 export class UserForm extends React.PureComponent{
+	state = {
+		counter: 0,
+		name: ""
+	}
+
 	render(){
 		return (
 			<form>
-				<input type="text"/>
+				<input type="text" value={this.state.name} onChange={this.onNameChange}/>
+				<button type="button" onClick={this.onCounterClick}>Counted {this.state.counter} times</button>
 			</form>
 		)
+	}
+
+	onCounterClick = () => {
+		this.setState({
+			counter: this.state.counter + 1,
+		})
+	}
+	onNameChange = (evt) => {
+		this.setState({
+			name: evt.target.value
+		})
 	}
 
 	static propTypes = {
