b81afe8089d204b81f53742652bbd2480f39df46
Thomas <thomas.rudfoss@gmail.com>
2019-05-22 10:28:10 +0200

    Constructor added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from "react"
 
 export class App extends React.PureComponent {
+	constructor(props) {
+		super(props)
+		window.app = this
+	}
+
 	render() {
 		return (
 			<div>Hello world</div>
