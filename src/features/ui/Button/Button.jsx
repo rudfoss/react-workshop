1e3552d3817f5bfb10086969d023ef3af2170416
Thomas Rudfoss <thomas.rudfoss@dnvgl.com>
2019-10-02 15:01:17 +0200

    Classname support

--- src/features/ui/Button/Button.jsx
+++ src/features/ui/Button/Button.jsx
@@ -4,7 +4,7 @@ import PropTypes from "prop-types"
 import classes from "./Button.scss"
 
 export const Button = ({ children, mode, ...props, }) => (
-	<button {...props} className={`${classes.button} ${props.className || ""}`} data-mode={mode}>{children}</button>
+	<button {...props} className={`${classes.button} ${props.className}`} data-mode={mode}>{children}</button>
 )
 Button.propTypes = {
 	mode: PropTypes.oneOf([
@@ -14,7 +14,8 @@ Button.propTypes = {
 	children: PropTypes.node
 }
 Button.defaultProps = {
-	mode: "primary"
+	mode: "primary",
+	className: ""
 }
 
 export default Button
