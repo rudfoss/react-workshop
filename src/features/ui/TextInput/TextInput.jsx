a3e4a8e229d82be84a9f4aceac680d1a17ad155b
Thomas Rudfoss <thomas.rudfoss@dnvgl.com>
2019-10-02 15:01:07 +0200

    Formatting

--- src/features/ui/TextInput/TextInput.jsx
+++ src/features/ui/TextInput/TextInput.jsx
@@ -6,15 +6,24 @@ const labelToId = (label) => label.replace(/[^a-z0-9-_]/ig, "").toLowerCase()
 
 import classes from "./TextInput.scss"
 
-export const TextInput = ({ id, label, type, value, onChange, ...rest }) => {
+export const TextInput = ({ id, label, type, className, value, onChange, ...rest }) => {
 	id = id || labelToId(label)
 	return (
-		<div className={classes.formField}>
+		<div className={`${classes.formField} ${className}`}>
 			<label htmlFor={id}>{label}</label>
 			{type === "textarea" ? (
-				<textarea id={id} value={value} onChange={onChangeHandler(onChange)} {...rest}></textarea>
+				<textarea
+					{...rest}
+					id={id}
+					value={value}
+					onChange={onChangeHandler(onChange)}></textarea>
 			) : (
-				<input id={id} type={type} value={value} onChange={onChangeHandler(onChange)} {...rest}/>
+				<input
+					{...rest}
+					id={id}
+					type={type}
+					value={value}
+					onChange={onChangeHandler(onChange)}/>
 			)}
 		</div>
 	)
@@ -22,12 +31,14 @@ export const TextInput = ({ id, label, type, value, onChange, ...rest }) => {
 TextInput.propTypes = {
 	id: PropTypes.string,
 	type: PropTypes.oneOf(["text", "password", "textarea"]),
+	className: PropTypes.string,
 
 	label: PropTypes.string.isRequired,
 	value: PropTypes.string,
 	onChange: PropTypes.func.isRequired
 }
 TextInput.defaultProps = {
+	className: "",
 	type: "text",
 	value: ""
 }
