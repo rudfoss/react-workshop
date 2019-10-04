e0595c2b9f6908914abfdb097758e46e1ad809a3
Thomas Rudfoss <thomas.rudfoss@dnvgl.com>
2019-10-04 14:25:53 +0200

    login saga

--- src/features/Login/login.duck.js
+++ src/features/Login/login.duck.js
@@ -1,4 +1,7 @@
 import { createAction, handleActions } from "redux-actions"
+import { takeLeading, select, put } from "redux-saga/effects"
+
+import * as usersDuck from "ducks/users.duck"
 
 const _ns = "login"
 export const getState = state => state[_ns] || {}
@@ -10,6 +13,15 @@ export const getUsername = (state) => getState(state).username || ""
 export const getPassword = (state) => getState(state).password || ""
 
 export const loginUser = action("LOGIN_USER")
+function* loginUserSaga() {
+	const username = yield select(getUsername)
+	const password = yield select(getPassword)
+	yield put(usersDuck.loginUser({ username, password }))
+}
+
+function* saga() {
+	yield takeLeading(loginUser, loginUserSaga)
+}
 
 export const reducer = handleActions({
 	[setUsername]: (state, { payload }) => ({
@@ -24,5 +36,6 @@ export const reducer = handleActions({
 
 export const duck = {
 	ns: _ns,
-	reducer
+	reducer,
+	saga
 }
