f083cf8653437578baaf559351f97995192cc24d
Thomas Haugland Rudfoss <thomas.rudfoss@gmail.com>
2019-11-27 07:17:00 +0100

    Added code link to home

--- src/features/Home/Home.jsx
+++ src/features/Home/Home.jsx
@@ -4,6 +4,8 @@ export const Home = () => (
 	<div className="jumbotron">
 		<h1 className="display-4">Welcome to the React/Redux Workshop</h1>
 		<p className="lead">This is the framework application we will use throughout the course to build React and Redux components</p>
+		<hr className="my-4"/>
+		<p>Code can be found here: <a href="https://github.com/rudfoss/react-workshop">https://github.com/rudfoss/react-workshop</a></p>
 	</div>
 )
 
