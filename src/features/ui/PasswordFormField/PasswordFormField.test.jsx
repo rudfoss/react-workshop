b7367a5467a552cd8b43149ffb068bbb9ee71a33
Thomas Haugland Rudfoss <thomas.rudfoss@gmail.com>
2019-11-27 19:59:05 +0100

    Password field test

---
+++ src/features/ui/PasswordFormField/PasswordFormField.test.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+import { PasswordFormField } from "./PasswordFormField"
+import TestRenderer from "react-test-renderer"
+
+describe("PasswordFormField", () => {
+	it("is defined", () => {
+		expect(PasswordFormField).toBeDefined()
+	})
+	it(`renders and input field of type "password"`, () => {
+		const result = TestRenderer.create(<PasswordFormField canShowPassword={false} label="bork" value="" onChange={() => 0}/>)
+		const root = result.root
+		const passwordInput = root.findByType("input")
+		expect(passwordInput.props.type).toEqual("password")
+	})
+})
