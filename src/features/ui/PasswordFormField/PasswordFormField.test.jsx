8da5eab37596a4728a2b24ee6a9ba8babe4d4e54
Thomas Haugland Rudfoss <thomas.rudfoss@gmail.com>
2019-11-27 20:04:24 +0100

    Improved password tests

--- src/features/ui/PasswordFormField/PasswordFormField.test.jsx
+++ src/features/ui/PasswordFormField/PasswordFormField.test.jsx
@@ -7,7 +7,7 @@ describe("PasswordFormField", () => {
 	it("is defined", () => {
 		expect(PasswordFormField).toBeDefined()
 	})
-	it(`renders and input field of type "password"`, () => {
+	it('renders an input field of type "password"', () => {
 		const result = TestRenderer.create(<PasswordFormField canShowPassword={false} label="bork" value="" onChange={() => 0}/>)
 		const root = result.root
 		const passwordInput = root.findByType("input")
