a8ac8507eab46b9bd7e639bd1c71b361c8136fd7
Thomas Haugland Rudfoss <thomas.rudfoss@gmail.com>
2019-11-27 10:39:56 +0100

    IncrementClass with odd/even placeholder

--- src/features/Increment/Increment.jsx
+++ src/features/Increment/Increment.jsx
@@ -4,4 +4,26 @@ const Increment = () => (
 	<div>Hello world</div>
 )
 
-export default Increment
+class IncrementClass extends React.PureComponent {
+	state = {
+		count: 0
+	}
+
+	increment = () => {
+		this.setState({
+			count: this.state.count + 1
+		})
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>{this.state.count}</h1>
+				<p>odd / even</p>
+				<button onClick={this.increment}>Increment</button>
+			</div>
+		)
+	}
+}
+
+export default IncrementClass
