dcf43818d337145beb4541c50b16cd52c58a9f7d
Thomas Haugland Rudfoss <thomas.rudfoss@gmail.com>
2019-11-29 11:15:55 +0100

    Removed unused duck references

--- src/ducks/index.js
+++ src/ducks/index.js
@@ -1,19 +1,7 @@
-import * as sagaTest from "../features/SagaTest/sagaTest.duck"
-
 export const duckMap = {
-	//[sagaTest.duck.ns]: sagaTest
-	/*[users.duck.ns]: users,
-	[createUser.duck.ns]: createUser,
-	[rooms.duck.ns]: rooms,
-	[room.duck.ns]: room,
-	[login.duck.ns]: login*/
+	/*[sagaTest.duck.ns]: sagaTest*/
 }
 
 export const ducks = [
-	//sagaTest.duck
-	/*users.duck,
-	createUser.duck,
-	rooms.duck,
-	room.duck,
-	login.duck*/
+	/*sagaTest.duck*/
 ]
